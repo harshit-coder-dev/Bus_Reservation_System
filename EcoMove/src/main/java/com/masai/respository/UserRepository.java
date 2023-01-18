9c588a6c56bd814b61139f12d1f06fe17ccd27d9
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-01-18 17:38:37 +0530

    added day-2 task

---
+++ EcoMove/src/main/java/com/masai/respository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.masai.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
