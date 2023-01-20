e32056b4d64be5c714a0288ba76125802a8c9035
Nishant Kumar <nkgunjan26102003@gmail.com>
2023-01-20 16:58:41 +0530

    added day-4 task

---
+++ EcoMove/src/main/java/com/masai/services/UserServiceImpl.java
@@ -0,0 +1,110 @@
+package com.masai.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.entities.CurrentAdminSession;
+import com.masai.entities.CurrentUserSession;
+import com.masai.entities.User;
+import com.masai.exceptions.AdminException;
+import com.masai.exceptions.UserException;
+import com.masai.respository.AdminSessionDao;
+import com.masai.respository.UserDao;
+import com.masai.respository.UserSessionDao;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private UserSessionDao userSessionDao;
+
+	@Autowired
+	private AdminSessionDao adminSessionDao;
+
+	@Override
+	public User createUser(User user) throws UserException {
+
+		User existingUser = userDao.findByContact(user.getContact());
+
+		if (existingUser != null)
+			
+			throw new UserException("User already registered with this Mobile number!");
+
+		return userDao.save(user);
+
+	}
+
+	@Override
+	public User updateUser(User user, String key) throws UserException {
+
+		CurrentUserSession loggedInUser = userSessionDao.findByUserUID(key);
+
+		if (loggedInUser == null) {
+			
+			throw new UserException("Please provide a valid key to update a User Details!");
+		}
+
+		if (user.getUserId() == loggedInUser.getUserId()) {
+
+			return userDao.save(user);
+			
+		} else
+			
+			throw new UserException("Invalid User Details! please login first.");
+	}
+
+	@Override
+	public User deleteUser(Integer userId, String key) throws UserException, AdminException {
+
+		CurrentAdminSession loggedInAdmin = adminSessionDao.findByAdminUID(key);
+
+		if (loggedInAdmin == null) {
+			
+			throw new AdminException("Please provide a valid key to delete user!");
+		}
+		
+		User user = userDao.findById(userId).orElseThrow(() -> new UserException("Invalid user Id!"));
+		userDao.delete(user);
+		
+		return user;
+
+	}
+
+	@Override
+	public User viewUserById(Integer userId, String key) throws UserException, AdminException {
+
+		CurrentAdminSession loggedInAdmin = adminSessionDao.findByAdminUID(key);
+
+		if (loggedInAdmin == null) {
+			
+			throw new AdminException("Please provide a valid key to view user datails!");
+		}
+
+		User user = userDao.findById(userId).orElseThrow(() -> new UserException("Invalid user Id!"));
+		return user;
+	}
+
+	@Override
+	public List<User> viewUsers(String key) throws UserException, AdminException {
+
+		CurrentAdminSession loggedInAdmin = adminSessionDao.findByAdminUID(key);
+		
+		if (loggedInAdmin == null) {
+			
+			throw new AdminException("Please provide a valid key to view all users!");
+		}
+
+		List<User> userList = userDao.findAll();
+		
+		if (userList.isEmpty())
+			
+			throw new UserException("No users found!");
+
+		return userList;
+	}
+}
